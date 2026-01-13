4309a7961426f201e462f951032c3ce995bf87be
sangram-jr <sangrambera58@gmail.com>
2026-01-13 16:39:01 +0530

    change wishlist component

--- amazon-cart/src/components/Cart/Cart.jsx
+++ amazon-cart/src/components/Cart/Cart.jsx
@@ -1,15 +1,10 @@
-import cartState from "../../store/cartItemState";
 
 
 const Cart=()=>{
-    //use the state
-    const cnt=cartState((state)=> state.count);
-    const setCnt=cartState((state)=>state.setCount);
 
     return(
         <div>
-          {cnt}
-          <button onClick={setCnt}>add</button>
+          cart
          
         </div>
     )
--- amazon-cart/src/components/Wishlist/Wishlist.jsx
+++ amazon-cart/src/components/Wishlist/Wishlist.jsx
@@ -1,8 +1,43 @@
+import { useState } from "react";
 
 const Wishlist=()=>{
+    const [item,setItem]=useState([
+        { 
+            id: 1, 
+            name: 'GFG T-shirt', 
+            price: 499, 
+            image:'https://media.geeksforgeeks.org/wp-content/uploads/20230823165506/gfg1.png'
+        },
+        { 
+            id: 2, 
+            name: 'GFG Bag', 
+            price: 699, 
+            image:'https://media.geeksforgeeks.org/wp-content/uploads/20230823165553/gfg2.jpg'
+        },
+        { 
+            id: 3, 
+            name: 'GFG Hoodie', 
+            price: 799, 
+            image:'https://media.geeksforgeeks.org/wp-content/uploads/20230823165623/gfg3.jpg'
+		}
+    ])
+
     return (
         <div>
-            wishlist
+            {
+                item.map((x)=>(
+                    <div key={x.id}>
+                        <img src={x.image}/>
+                        <div>
+                            <p>{x.name}</p>
+                            <p>{x.price}</p>
+                        </div>
+                        <div>
+                            <button>add to cart</button>
+                        </div>
+                    </div>
+                ))
+            }
         </div>
     )
 }
