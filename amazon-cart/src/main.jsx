42479db4f3902997208aff415048f6fcee9af06b
sangram-jr <sangrambera58@gmail.com>
2026-01-13 20:28:52 +0530

    wrap app component with UniversalStore component in main.jsx

--- amazon-cart/src/main.jsx
+++ amazon-cart/src/main.jsx
@@ -2,12 +2,17 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import UniversalStore from './store/context.jsx'
 
 
 createRoot(document.getElementById('root')).render(
   
   <StrictMode>
-    <App />
+    
+    <UniversalStore>
+      <App />
+    </UniversalStore>
+    
   </StrictMode>
     
 )
